c68bf1d09f584c19f931fa875cd2b4f6b00abc76
nikitha-56 <nikithatalluri1@gmail.com>
2025-09-05 01:43:27 +0530

    cors error rectified

--- src/main/java/com/example/notes/controller/NoteController.java
+++ src/main/java/com/example/notes/controller/NoteController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-@CrossOrigin(origins = "https://notes-frontend-app-murex.vercel.app")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/notes")
 public class NoteController {
